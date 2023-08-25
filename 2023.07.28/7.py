b12bc7f087b044053fc4a663562d23a12768204a
Gennadiy <renardes@gmail.com>
2023-08-25 10:07:08 +0500

    2023.07.28: комментарии, правки, итоги

--- 2023.07.28/7.py
+++ 2023.07.28/7.py
@@ -40,22 +40,32 @@ list_of_dicts = [
         'Тольятти': 2
     }
 ]
+
 cities_set = set()
 for i in range(len(list_of_dicts)):
     cities_set = cities_set | set(list_of_dicts[i])
+# ИСПОЛЬЗОВАТЬ: генераторное выражение
+cities_set = {city for cities_dict in list_of_dicts for city in cities_dict}
+
+# ИСПРАВИТЬ: функция sorted() сортирует элементы переданного итерируемого объекта по возрастанию — reverse=False можно опустить
 cities_values = dict.fromkeys(sorted(cities_set, reverse=False), set())
 
 for cities_dict in list_of_dicts:
     for city, val in cities_values.items():
+        # ИСПРАВИТЬ: использование словарных методов get() и setdefault() позволяет избежать явного перехвата исключений
         try:
+            # ИСПРАВИТЬ: на предложенном наборе данных эта проверка всегда вернёт True, следовательно в ней нет смысла
+            # КОММЕНТАРИЙ: перехват исключения может выполняться и без проверки, в любом выражении
             if cities_dict[city]:
                 try:
                     cities_values[city] = cities_values[city] | {str(cities_dict[city])}
                 except KeyError:
+                    # УДАЛИТЬ: данное выражение не используется
                     cities_values[city]
         except KeyError:
+            # УДАЛИТЬ: данное выражение не используется
             cities_values[city]
-            
+
 for k, v in cities_values.items():
     print(f'{k}: {v}')
 
@@ -79,6 +89,9 @@ for k, v in cities_values.items():
 # Уфа: {'3', '7'}
 # Хабаровск: {'2'}
 # Ярославль: {'1'}
+
+
+# ИТОГ: хорошо, но можно лучше — 3/5
 
 
 # ИСПОЛЬЗОВАТЬ везде: PEP 8 не рекомендует добавлять пробелы вокруг = при передаче аргументов по ключу
--- 2023.07.28/8.py
+++ 2023.07.28/8.py
@@ -1,26 +1,35 @@
 files = input('Введите наименования файлов: ')
+# ИСПРАВИТЬ: строковый метод split() всегда возвращает объект list — преобразование в list избыточно
 files = list(files.split('; '))
 
+# ПЕРЕИМЕНОВАТЬ: file — файл (ед.ч.); files — файлы (мн.ч.)
+# КОММЕНТАРИЙ: текущие имена списков могут навести на мысль, что в каждом списке находится содержимое одного файла — необходимо давать переменным более однозначные имена
 midle_file = []
 final_file = []
 
+# ИСПРАВИТЬ: вместо использования двух списков куда более продуктивно создать множество из имён файлов, по которому затем итерироваться, а количество файлов проверять в исходном списке файлов
 for file in files:
     midle_file.append(file)
     if midle_file.count(file) > 1:
         counter = midle_file.count(file)
+        # ИСПРАВИТЬ: для получения строки в обратном направлении достаточно взять срез [::-1] — от конца строки до начала строки с шагом -1
         file = list(file)
         file.reverse()
+        # ИСПРАВИТЬ: используйте срезы строк и строковые методы вместо явного использования логических переменных и цикла
         point_indicator = False
         for i in range(len(file)):
+            # ИСПРАВИТЬ: после ключевых слов if и elif интерпретатор ожидает объект bool, а не оператор сравнения — вам не нужно сравнивать объект bool с объектом False
             if file[i-1] == '.' and point_indicator == False:
                 point_indicator = True
                 file[i-1] = '_' + str(counter) + '.'
         file.reverse()
         file = ''.join(file)
+        # ИСПРАВИТЬ: данное действие выполняется одинаковым образом вне зависимости от результата проверки условия — вынести за пределы условной конструкции
         final_file.append(file)
     else:
         final_file.append(file)
 
+# ИСПРАВИТЬ: условие задачи требует вывести новые имена файлов по алфавиту
 for ff in final_file:
     print(ff)
 
@@ -37,3 +46,15 @@ for ff in final_file:
 # main_3.cpp
 # src.tar_2.gz
 
+
+# ИТОГ: переработать — 2/6
+
+
+# ИСПОЛЬЗОВАТЬ: изучите более оптимальный вариант решения
+files = input().split('; ')
+files_renamed = []
+for file in set(files):
+    for cnt in range(1, files.count(file)+1):
+        suffix = f'_{cnt}.' if cnt > 1 else '.'
+        files_renamed.append(suffix.join(file.split('.', 1)))
+print(*sorted(files_renamed), sep='\n')
