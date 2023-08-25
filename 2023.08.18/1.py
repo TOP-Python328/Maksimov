66079e84c71fb6ea05b8f39d4ab5e0be228dce7c
innokenty <m94kesha@mail.ru>
2023-08-25 21:40:38 +0900

    код оптимизирован

--- 2023.08.18/1.py
+++ 2023.08.18/1.py
@@ -1,34 +1,44 @@
 def strong_password(password: str) -> bool:
-    border_checkpoint = {'big_r': False, 'lower_r': False, 'num_two': 0, 'other_signs': False}
-    if len(password) < 8:
+
+    upper_case = False # Вычисление латинских букв нижнего регистра в пароле;
+    for simvol in password:
+        if ord(simvol) >= 65 and ord(simvol) <= 90:
+            upper_case = True
+            break
+    lowercase = False # Вычисление латинских букв ВЕРХНЕГО регистра в пароле;
+    for simvol in password:
+        if ord(simvol) >= 97 and ord(simvol) <= 122:
+            lowercase = True
+            break
+    counting_numbers = 0 # Подсчет количества цыфр в пароле;
+    for simvol in password:
+        if ord(simvol) >= 48 and ord(simvol) <= 57:
+            counting_numbers += 1
+    other_signs = False # Вычисление символов прочих категорий (пробел, знаки пунктуации, диакритические знаки и т.п.);
+    for simvol in password:
+        if (ord(simvol) >= 32 and ord(simvol) <= 47
+            or ord(simvol) >= 58 and ord(simvol) <= 64
+            or ord(simvol) >= 91 and ord(simvol) <= 96
+            or ord(simvol) >= 123 and ord(simvol) <= 126
+        ):
+            other_signs = True
+            break
+
+    if len(password) < 8: # Проверка длины пароля (менее восьми);
         return False
-    else:
-        for simvol in password:
-            if (ord(simvol) >= 65 and ord(simvol) <= 90):
-                border_checkpoint['big_r'] = True
-                break
-        for simvol in password:
-            if (ord(simvol) >= 97 and ord(simvol) <= 122):
-                border_checkpoint['lower_r'] = True
-                break
-        if border_checkpoint['big_r'] == True and border_checkpoint['lower_r'] == True:
-            for simvol in password:
-                if (ord(simvol) >= 48 and ord(simvol) <= 57):
-                    border_checkpoint['num_two'] += 1
-            if border_checkpoint['num_two'] >= 2:
-                for simvol in password:
-                    if (ord(simvol) >= 32 and ord(simvol) <= 47) or (ord(simvol) >= 58 and ord(simvol) <= 64) or (ord(simvol) >= 91 and ord(simvol) <= 96) or (ord(simvol) >= 123 and ord(simvol) <= 126):
-                        border_checkpoint['other_signs'] = True
-                        break
-                return border_checkpoint['other_signs']
-            else:
-                return False
-        else:
-            return False
-            
-#C:\Users\Кеша\Desktop\my_life\Б. Прогаммирование\Top-Piton\HomeWork_Python_TOP\HW\2023.08.18>python -i 1.py
-#>>> strong_password('aP3:kD_l3')
-#True
-#>>> strong_password('password')
-#False
-#>>>
+    elif upper_case == False: # Проверка на отсутствие латинских букв ВЕРХНЕГО регистра;
+        return False
+    elif lowercase == False: # Проверка на отсутствие латинских букв нижнего регистра;
+        return False
+    elif counting_numbers < 2: # Проверка на отсутствие двух или более цифр;
+        return False
+    elif other_signs == False: # Проверка на отсутствие символов прочих категорий (пробел, знаки пунктуации, диакритические знаки и т.п.);
+        return False
+    elif other_signs: # Проверка на НАЛИЧИЕ символов прочих категорий (пробел, знаки пунктуации, диакритические знаки и т.п.);
+        return True
+                 
+# >>> strong_password('aP3:kD_l3')
+# True
+# >>> strong_password('password')
+# False
+# >>>
