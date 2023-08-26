b83b076b9c37b0f46183273a93d248ef4c6144b5
innokenty <m94kesha@mail.ru>
2023-08-26 23:17:28 +0900

    Задание 6

---
+++ 2023.08.18/6.py
@@ -0,0 +1,33 @@
+def orth_triangle(cathetus1: float=0, cathetus2: float=0, hypotenuse: float=0,) -> float | None:
+    
+    if (cathetus1 < 0 # Проверка на возможность вычисления по Теореме Пифагора;
+        or cathetus2 < 0
+        or hypotenuse < 0
+        or hypotenuse < cathetus1 and hypotenuse > 0 and cathetus1 > 0
+        or hypotenuse < cathetus2 and hypotenuse > 0 and cathetus2 > 0
+        or cathetus1 != 0 and cathetus2 != 0 and hypotenuse != 0
+        ):
+        return None
+    elif not hypotenuse: # Расчет при известных переменных cathetus1 и cathetus2;
+        hypotenuse = (cathetus1 ** 2 + cathetus2 ** 2) ** (1 / 2)
+        return hypotenuse
+    elif not cathetus1: # Расчет при известных переменных cathetus2 и hypotenuse;
+        cathetus1 = (hypotenuse ** 2 - cathetus2 ** 2) ** (1 / 2)
+        return cathetus1
+    elif not cathetus2: # Расчет при известных переменных cathetus1 и hypotenuse;
+        cathetus2 = (hypotenuse ** 2 - cathetus1 ** 2) ** (1 / 2)
+        return cathetus2
+ 
+ 
+# >>> orth_triangle(cathetus1=3, hypotenuse=5)
+# 4.0
+# >>> orth_triangle(cathetus1=8, cathetus2=15)
+# 17.0
+# >>> print(orth_triangle(cathetus2=9, hypotenuse=3))
+# None
+
+# >>> print(orth_triangle(cathetus2=-2, hypotenuse=3))
+# None
+
+# >>> print(orth_triangle(cathetus1=1, cathetus2=2, hypotenuse=3))
+# None
