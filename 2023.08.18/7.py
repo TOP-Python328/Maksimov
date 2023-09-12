d66d5830457a27aa107b2d0ee87f48cbdf1ee65d
Innokentii <m94kesha@mail.ru>
2023-09-12 21:26:42 +0900

    Задание-7 Замечания исправлены

--- 2023.08.18/7.py
+++ 2023.08.18/7.py
@@ -1,61 +1,51 @@
-# СДЕЛАТЬ: реализация поставленной задачи с помощью одной функции неэффективна — об этом было сказано в условии задачи
+num_sys_36 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+dict_num = dict([*zip(num_sys_36, range(0, 36))])
+dict_num_two = dict([*enumerate(num_sys_36)])
+
 def int_base(number: str, this_num_sys: int, num_sys_con: int) -> str | None:
-    # ДОБАВИТЬ: документацию функции
+    """Функция преобразовывает число из произвольной системы счисления в число десятичной системы исчисления"""
     number = number.lower()
-    # УДАЛИТЬ: создавать словари при каждом вызове функции избыточно, замедляет работу функции — переместите этот фрагмент кода за пределы функции
-    # ИСПРАВИТЬ: не так сложно написать код, генерирующий нужные последовательности символов и чисел
-    num_sys_36 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    num_sys_0 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-    # ИСПРАВИТЬ: создание списка с распаковкой в его элементы возвращаемого значения функции zip() избыточно — функция dict() работает с любыми итерируемыми объектами, включая объекты генераторы, возвращаемые zip()
-    dict_num = dict([*zip(num_sys_36, num_sys_0)])
-    dict_num_two = dict([*zip(num_sys_0, num_sys_36)])
+
     # Проверка на коректности ввода данных с системой исчисления;
     for n in number:
         if dict_num[n] > this_num_sys:
-            # ИСПРАВИТЬ: функция не должна самостоятельно выводить что-либо в stdout
-            return print(None)
+            return None
     
     # Преобразование числа n-го исчисления в десятичный;
-    # КОММЕНТАРИЙ: рекомендовано использование встроенной функции enumerate()
-    # ИСПРАВИТЬ: многократный вызов функции len() на каждой итерации цикла генераторного выражения избыточен, замедляет выполнение кода
-    gen_work_num = (dict_num[number[i]] * (this_num_sys ** (len(number) - 1 - i)) for i in range(len(number)))
+    len_num = len(number)
+    gen_work_num = (dict_num[number[i]] * (this_num_sys ** (len_num - 1 - i)) for i in range(len_num))
     ten_num = sum(gen_work_num) # - десятичное число;
     
     # Создание массива данных по разряду числа n-го исчисления;
-    # ПЕРЕИМЕНОВАТЬ: discharge — разряд (электр.) аккумулятора; разряд (матем.) числа — rank, digits
-    discharge = 0 # - разряжы результирующего данного;
-    work_div = ten_num # - рабочая сумма для определения разрядов резултирующего данного;
+    rank = 0
+    work_div = ten_num
+    result_number_key = []
     while True:
-        # КОММЕНТАРИЙ: рекомендовано использование встроенной функции divmod()
         work_div = work_div // num_sys_con
-        discharge += 1
+        result_number_key.append(int(0))
+        rank += 1
         if work_div == 0:
             break
-    result_number_key = []
-    # УДАЛИТЬ: этот и последующий циклы избыточны
-    for n in range(discharge):
-        result_number_key.append(int(0))
-    # Создание ключей числа n-го исчисления;
+    i = 1
+    return result_funk(result_number_key, ten_num, num_sys_con)
+    
+def result_funk(res_num: list[int], ten_num: int, num_sys_con: int):
+    """Функция преобразует десятичное число в произвольную систему счисления число"""
     for ch in range(ten_num):
-        result_number_key[0] += 1
+        res_num[0] += 1
         i = 1
-        while i < len(result_number_key):
-            if result_number_key[i-1] == num_sys_con:
-                result_number_key[i-1] = 0
-                result_number_key[i] += 1
+        while i < len(res_num):
+            if res_num[i-1] == num_sys_con:
+                res_num[i-1] = 0
+                res_num[i] += 1
             i += 1
     # Результат;
-    return ''.join((dict_num_two[key] for key in result_number_key[::-1]))
+    return ''.join((dict_num_two[key] for key in res_num[::-1]))
 
 
 # >>> int_base('ff00', 16, 2)
 # '1111111100000000'
 # >>> int_base('1101010', 2, 30)
 # '3g'
-# >>> int_base('12345', 3, 30)
+# >>> print(int_base('12345', 3, 30))
 # None
-
-
-# ИТОГ: хорошо, что попытались, но над эффективностью кода надо работать, а для этого надо развивать аналитический образ мышления и внимательно изучать большое количество примеров кода
-
-# КОММЕНТАРИЙ: этот ресурс поможет вам лучше придумывать имена переменным: https://context.reverso.net/перевод/русский-английский
