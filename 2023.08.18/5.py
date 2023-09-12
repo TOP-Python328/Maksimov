71ecf6a37cce2bdfbdf6e0826155a408ff67c461
Innokentii <m94kesha@mail.ru>
2023-09-12 17:43:59 +0900

    Задание-5 Замечания исправлены

--- 2023.08.18/5.py
+++ 2023.08.18/5.py
@@ -1,31 +1,30 @@
-# ИСПРАВИТЬ: аргументами должны быть вещественные числа
-def central_tendency(number_one: int, number_two: int, *numbers: int) -> dict[str, float]:
-    # ДОБАВИТЬ: документацию функции
-    # ПЕРЕИМЕНОВАТЬ: используйте идентификатор numbers
-    work_numbers = [number_one, number_two, *numbers]
+def central_tendency(number_one: float, number_two: float, *numbers_: float) -> dict[str, float]:
+    """Функция вычисляет основные меры центральной тенденции для некоторого количества чисел"""
+    numbers = [number_one, number_two, *numbers_]
     central_tendency_result = {'median': float,
                                'arithmetic': float,
                                'geometric': float,
                                'harmonic': float,
                                }
     # расчет медианы;
-    # ИСПРАВИТЬ: для того, чтобы вычислять медиану таким образом (в обоих случаях ниже), последовательность чисел должна быть отсортирована (см. тест ниже)
-    if len(work_numbers) % 2 == 0:
-        # ИСПРАВИТЬ: многократные вычисления одних и тех же значений избыточны, заметно замедляют выполнение функции
-        central_tendency_result['median'] = (work_numbers[int(len(work_numbers) / 2)] + work_numbers[int((len(work_numbers) / 2) - 1)]) / 2
+    numbers = sorted(numbers, reverse=True)
+    if len(numbers) % 2 == 0:
+        num_s = len(numbers) / 2
+        central_tendency_result['median'] = (numbers[int(num_s)] + numbers[int((num_s) - 1)]) / 2
     else:
-        central_tendency_result['median'] = float(work_numbers[len(work_numbers) // 2])
+        central_tendency_result['median'] = float(numbers[len(numbers) // 2])
     # расчет среднюю арифметическую;
-    # ИСПРАВИТЬ: в Python не используется символ ;
-    central_tendency_result['arithmetic'] = sum(n for n in work_numbers) / len(work_numbers);
+    central_tendency_result['arithmetic'] = sum(n for n in numbers) / len(numbers)
     # расчет среднюю геометрическую;
     res_multip_ = 1
-    for n in work_numbers:
+    for n in numbers:
         res_multip_ = res_multip_ * n
-    central_tendency_result['geometric'] = res_multip_ ** (1 / len(work_numbers))
+    central_tendency_result['geometric'] = res_multip_ ** (1 / len(numbers))
     # расчет среднюю гармоническую;
-    # ИСПРАВИТЬ: вычисление среднего гармонического невозможно для последовательности, содержащей ноль (см. тест ниже)
-    central_tendency_result['harmonic'] = len(work_numbers) / sum(1 / n for n in work_numbers);
+    try:
+        central_tendency_result['harmonic'] = len(numbers) / sum(1 / n for n in numbers if not n == 0)
+    except:
+        central_tendency_result['harmonic'] = 0
     return central_tendency_result
 
 
@@ -34,24 +33,17 @@ def central_tendency(number_one: int, number_two: int, *numbers: int) -> dict[st
 # >>> sample = [1, 2, 3, 4, 5]
 # >>> central_tendency(*sample)
 # {'median': 3.0, 'arithmetic': 3.0, 'geometric': 2.605171084697352, 'harmonic': 2.18978102189781}
-
+# >>>
 # >>> central_tendency(1)
 # Traceback (most recent call last):
   # File "<stdin>", line 1, in <module>
 # TypeError: central_tendency() missing 1 required positional argument: 'number_two'
-
+# >>>
 # >>> central_tendency(1, 2)
 # {'median': 1.5, 'arithmetic': 1.5, 'geometric': 1.4142135623730951, 'harmonic': 1.3333333333333333}
-
+# >>>
 # >>> central_tendency(2, 5, 1, 4, 3)
-# КОММЕНТАРИЙ: медиана должна быть равна 3.0
-# {'median': 1.0, 'arithmetic': 3.0, 'geometric': 2.605171084697352, 'harmonic': 2.18978102189781}
-
+# {'median': 3.0, 'arithmetic': 3.0, 'geometric': 2.605171084697352, 'harmonic': 2.18978102189781}
+# >>>
 # >>> central_tendency(1, 0, -1)
-# ...
-# ZeroDivisionError: division by zero
-
-
-# ИТОГ: переделать — 1/6
-
-# КОММЕНТАРИЙ: минус балл за несоблюдение требований оформления в соответствии с PEP 8 — несмотря на многочисленные указания и примеры, предоставленные преподавателем
+# {'median': 0.0, 'arithmetic': 0.0, 'geometric': 0.0, 'harmonic': 0}
