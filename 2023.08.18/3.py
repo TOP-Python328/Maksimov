f1218925994b4c2af49e4e697bfc75ec96ede3ad
Innokentii <m94kesha@mail.ru>
2023-09-12 15:27:47 +0900

    Задание-3 Замечания исправлены

--- 2023.08.18/3.py
+++ 2023.08.18/3.py
@@ -1,18 +1,15 @@
-# ИСПРАВИТЬ: параметр copy должен быть строго ключевым
 # ИСПРАВИТЬ: функция возвращает список вещественных чисел, а не произвольных объектов — это должно быть отражено в аннотации
-def numbers_strip(numbers: list[float], n: int=1, copy=False) -> list:
-    # ДОБАВИТЬ: документацию функции
-    if copy: # если copy=True то функция возвращает копию исходного объекта;
-        # ПЕРЕИМЕНОВАТЬ: используйте идентификатор numbers
-        work_object = numbers.copy()
-    # УДАЛИТЬ: данный блок избыточен
-    else: # если copy=False то функция возвращает итерированный исходный объект;
-        work_object = numbers
-    
+def numbers_strip(numbers: list[float], n: int=1, copy_: bool=False) -> list:
+    """Функция удаляет n минимальных и n максимальных чисел из списка"""
+    # если copy=False то функция возвращает итерированный исходный объект;
+    # если copy=True то функция возвращает копию исходного объекта;
+    if copy_:
+        numbers = numbers.copy()
+
     for _ in range(n): # Удаление Максимальных и минимальных значений
-        work_object.remove(min(work_object))
-        work_object.remove(max(work_object))
-    return work_object
+        numbers.remove(min(numbers))
+        numbers.remove(max(numbers))
+    return numbers
 
 
 # >>> sample = [1, 2, 3, 4]
@@ -22,23 +19,17 @@ def numbers_strip(numbers: list[float], n: int=1, copy=False) -> list:
 # >>> sample is sample_stripped
 # True
 # >>> id(sample)
-# 2966653841600
+# 2339904526720
 # >>> id(sample_stripped)
-# 2966653841600
+# 2339904526720
 # >>>
 # >>> sample = [10, 20, 30, 40, 50]
-# >>> sample_stripped = numbers_strip(sample, 2, copy=True)
+# >>> sample_stripped = numbers_strip(sample, 2, copy_=True)
 # >>> sample_stripped
 # [30]
 # >>> sample is sample_stripped
 # False
 # >>> id(sample)
-# 2966653840704
+# 2339904525824
 # >>> id(sample_stripped)
-# 2966653839360
-# >>>
-
-
-# ИТОГ: нужно лучше, доработать — 1/4
-
-# КОММЕНТАРИЙ: минус балл за несоблюдение требований оформления в соответствии с PEP 8 — несмотря на многочисленные указания и примеры, предоставленные преподавателем
+# 2339904524480
