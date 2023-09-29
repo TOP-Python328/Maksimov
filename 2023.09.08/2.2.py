bb64b97b90dea73e57681b609b22ff588972a6a9
Innokentii <m94kesha16mak@mail.ru>
2023-09-29 23:45:10 +0600

    Задание-2.2

---
+++ 2023.09.08/2.2.py
@@ -0,0 +1,14 @@
+def tree_leaves(leaves) -> int:
+    q = leaves[:]
+    count = 0
+    while q:
+        entry = q.pop()
+        if isinstance(entry, list):
+            q += entry
+        if entry == 'leaf': count += 1
+    return count
+        
+# >>> tree = [[[['leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf', 'leaf'], 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf']], [['leaf', 'leaf'], ['leaf', 'leaf', 'leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf'], ['leaf', 'leaf', ['leaf', 'leaf', 'leaf']], 'leaf', 'leaf'], ['leaf', 'leaf', ['leaf', 'leaf'], 'leaf']]
+# >>>
+# >>> tree_leaves(tree)
+# 38
